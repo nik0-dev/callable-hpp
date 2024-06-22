dd4d998f029032d28aef379d5831830de0b9086d
Isabelle <121070808+nik0-dev@users.noreply.github.com>
2024-06-21 19:15:24 -0500

    fixed formatting on callable.hpp

--- callable.hpp
+++ callable.hpp
@@ -5,15 +5,15 @@ template <typename Return, typename ... Args>
 class Callable {
     using FunctionVariant = Return(*)(Args...);
     
-    private:
-        FunctionVariant func = nullptr;
+private:
+    FunctionVariant func = nullptr;
 
-    public:
-        Callable() {} 
-        Callable(FunctionVariant f) : func(f) {}
-        void Bind(FunctionVariant f) { func = f; }
-        Return Call(Args... args) { return IsValid() ? func(args...) : Return{}; }
-        bool IsValid() { return func != nullptr; }
+public:
+    Callable() {} 
+    Callable(FunctionVariant f) : func(f) {}
+    void Bind(FunctionVariant f) { func = f; }
+    Return Call(Args... args) { return IsValid() ? func(args...) : Return{}; }
+    bool IsValid() { return func != nullptr; }
 };
 
 #endif
