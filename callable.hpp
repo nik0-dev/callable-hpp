7db3764edb840f05ee688da5cec10cbdffa89ed8
Isabelle <121070808+nik0-dev@users.noreply.github.com>
2024-06-21 19:14:52 -0500

    add callable.hpp

---
+++ callable.hpp
@@ -0,0 +1,19 @@
+#ifndef CALLABLE_HPP
+#define CALLABLE_HPP
+
+template <typename Return, typename ... Args>
+class Callable {
+    using FunctionVariant = Return(*)(Args...);
+    
+    private:
+        FunctionVariant func = nullptr;
+
+    public:
+        Callable() {} 
+        Callable(FunctionVariant f) : func(f) {}
+        void Bind(FunctionVariant f) { func = f; }
+        Return Call(Args... args) { return IsValid() ? func(args...) : Return{}; }
+        bool IsValid() { return func != nullptr; }
+};
+
+#endif
